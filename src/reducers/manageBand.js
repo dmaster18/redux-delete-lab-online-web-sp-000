67712f069e3a433c41f6c4ca1eea253cc4aa6135
David Strauch <dmaster18@gmail.com>
2021-01-23 20:22:43 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -5,18 +5,17 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-    
-    const band = {
-      id: uuid(),
-      name: action.name
-    }
-
-    return { bands: state.bands.concat(band) };
-
+      const band = {
+        id: uuid(),
+        name: action.name
+      }
+      return { bands: state.bands.concat(band) };
+      
     case 'DELETE_BAND':
-      return { bands: state.bands.filter(band => band.id !== action.id) };
-    
-      default:
+
+      return {bands: state.bands.filter(band => band.id !== action.id)}
+
+    default:
       return state;
   }
 };
